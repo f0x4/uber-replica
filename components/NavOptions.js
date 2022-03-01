ae340085fce217f91313e175281cae5742085a00
f0x4 <70147253+f0x4@users.noreply.github.com>
2022-03-01 18:18:19 +0300

    React Navigation and React Native Elements installed and implemented

--- components/NavOptions.js
+++ components/NavOptions.js
@@ -1,31 +1,36 @@
 import { View, Text, FlatList, TouchableOpacity, Image } from "react-native";
 import React from "react";
 import tw from "tailwind-react-native-classnames";
+import { Icon } from "react-native-elements";
+import { useNavigation } from "@react-navigation/native";
 
 const navItems = [
     {
         id: "000",
         title: "Get a ride",
         image: require("../assets/car.webp"),
-        screen: "MapScrren",
+        screen: "MapScreen",
     },
     {
         id: "001",
         title: "Order food",
         image: require("../assets/food.png"),
-        screen: "EatsScrren",
+        screen: "EatsScreen",
     },
 ];
 
 const NavOptions = () => {
+    const navigation = useNavigation();
+
     return (
         <FlatList
             data={navItems}
             horizontal
             keyExtractor={(item) => item.id}
-            renderItem={({ item, index }) => {
+            renderItem={({ item }) => {
                 return (
                     <TouchableOpacity
+                        onPress={() => navigation.navigate(item.screen)}
                         style={tw`p-2 pl-6 pb-7 pt-4 bg-gray-200 m-2 w-40 rounded`}
                     >
                         <View>
@@ -37,10 +42,16 @@ const NavOptions = () => {
                                     resizeMode: "contain",
                                 }}
                             />
+                            <Text style={tw`mt-2 text-lg font-semibold`}>
+                                {item.title}
+                            </Text>
                         </View>
-                        <Text style={tw`mt-2 text-lg font-semibold`}>
-                            {item.title}
-                        </Text>
+                        <Icon
+                            style={tw`p-2 bg-black rounded-full w-10 mt-4`}
+                            name="arrowright"
+                            color="white"
+                            type="antdesign"
+                        />
                     </TouchableOpacity>
                 );
             }}
