c49ed0a4496f44532449e6b66deb5356a3cf033a
f0x4 <70147253+f0x4@users.noreply.github.com>
2022-03-10 14:27:22 +0300

    Map componet and .env.example created

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -4,8 +4,12 @@ import tw from "tailwind-react-native-classnames";
 import NavOptions from "../components/NavOptions";
 import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
 import { GOOGLE_MAPS_APIKEY } from "@env";
+import { useDispatch } from "react-redux";
+import { setDestination, setOrigin } from "../slices/navSlice";
 
 const HomeScreen = () => {
+    const dispatch = useDispatch();
+
     return (
         <SafeAreaView style={tw`bg-white h-full`}>
             <View style={tw`p-5`}>
@@ -19,6 +23,19 @@ const HomeScreen = () => {
                         textInput: { fontSize: 18 },
                     }}
                     placeholder="Were From?"
+                    onPress={(data, details = null) => {
+                        dispatch(
+                            setOrigin({
+                                location: details.geometry.location,
+                                description: data.description,
+                            })
+                        );
+                        dispatch(setDestination(null));
+                    }}
+                    fetchDetails={true}
+                    returnKeyType={"search"}
+                    enablePoweredByContainer={false}
+                    minLength={2}
                     nearbyPlacesAPI="GooglePlacesSearch"
                     query={{ key: GOOGLE_MAPS_APIKEY, language: "en" }}
                     debounce={1500}
--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,10 +1,36 @@
-import { StyleSheet, Text, View } from "react-native";
+import { createStackNavigator } from "@react-navigation/stack";
 import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import tw from "tailwind-react-native-classnames";
+import Map from "../components/Map";
 
 const MapScreen = () => {
+    const Stack = createStackNavigator();
     return (
         <View>
             <Text>MapScreen</Text>
+            <View style={tw`h-1/2`}>
+                <Map />
+            </View>
+
+            <View style={tw`h-1/2`}>
+                <Stack.Navigator>
+                    <Stack.Screen
+                        name="NavigateCard"
+                        component={NavOptions}
+                        options={{
+                            headerShown: false,
+                        }}
+                    ></Stack.Screen>
+                    <Stack.Screen
+                        name="RideOptionsCard"
+                        component={NavOptions}
+                        options={{
+                            headerShown: false,
+                        }}
+                    ></Stack.Screen>
+                </Stack.Navigator>
+            </View>
         </View>
     );
 };
